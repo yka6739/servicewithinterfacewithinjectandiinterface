e24f00453c87d571b5db89587eba6b9926ad4b05
marsal <marsal@DESKTOP-SL3HUAK>
2021-07-10 19:25:26 +0530

    Add project files.

---
+++ servicewithinterfacewithinjectandiinterface/Client/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using servicewithinterfacewithinjectandiinterface.Client.Resounces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace servicewithinterfacewithinjectandiinterface.Client.Pages
+{
+    public partial class Index
+    {
+        private List<Data> d;
+        protected override void OnInitialized()
+        {
+            d = ccc.GetData();
+        }
+
+    }
+}
---
+++ servicewithinterfacewithinjectandiinterface/Client/Shared/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using servicewithinterfacewithinjectandiinterface.Client.Resounces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace servicewithinterfacewithinjectandiinterface.Client.Shared
+{
+    public class MyClass : MyInterFace
+
+    {
+        public List<Data> GetData()
+        {
+            return new List<Data>()
+            {
+                new Data(){Id=101, Name="Yatendra", Address="Noida"}
+            };
+        }
+    }
+}
